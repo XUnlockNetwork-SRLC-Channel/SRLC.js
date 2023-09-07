41b23cce0203d73f01bda2440a6eb77f70fcafc0
XCader <100986647+luacoder279@users.noreply.github.com>
2023-09-06 21:59:04 -0700

    Create srlc.js

---
+++ srlc.js
@@ -0,0 +1,4 @@
+//https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css
+var s = document.createElement('script');s.type='text/javascript';s.src='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js';document.body.appendChild(s);
+var v = document.createElement('style');v.type='text/css';v.src='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css';document.body.appendChild(v);
+var r = document.createElement('style');r.type='text/css';r.src='https://www.w3schools.com/w3css/4/w3.css';document.body.appendChild(r);
